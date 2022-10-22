523a403dc544db9f6ef6030b7fd849d7947dfdc1
Peyton Moss <pmoss94@gmail.com>
2022-10-21 21:24:05 -0600

    rollbar finished - ready for deploy

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const {shuffleArray} = require('./utils')
 app.use(express.json())
 app.use(express.static('public'))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '053340723f7c44b9ba5360c370eda26d',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+//rollbar.log("Hello world!");
+
+
 /** HTML endpoint */
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -28,24 +40,28 @@ app.get('/api/robots', (req, res) => {
     try {
         res.status(200).send(botsArr)
     } catch (error) {
+        rollbar.critical('failure to get all robots')
         console.log('ERROR GETTING BOTS', error)
         res.sendStatus(400)
     }
 })
 
 app.get('/api/robots/five', (req, res) => {
+    rollbar.log('player got 5 robots')
     try {
         let shuffled = shuffleArray(bots)
         let choices = shuffled.slice(0, 5)
         let compDuo = shuffled.slice(6, 8)
         res.status(200).send({choices, compDuo})
     } catch (error) {
+        rollbar.error('error receiving 5 robots')
         console.log('ERROR GETTING FIVE BOTS', error)
         res.sendStatus(400)
     }
 })
 
 app.post('/api/duel', (req, res) => {
+    rollbar.log('dual started')
     try {
         // getting the duos from the front end
         let {compDuo, playerDuo} = req.body
@@ -67,6 +83,7 @@ app.post('/api/duel', (req, res) => {
             playerRecord.losses++
             res.status(200).send('You lost!')
         } else {
+            rollbar.debug('Player won')
             playerRecord.losses++
             res.status(200).send('You won!')
         }
